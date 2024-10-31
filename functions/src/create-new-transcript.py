857186e6879afec2da9e31ec2577769631b7fad9
ffex <kurogan21@gmail.com>
2024-10-31 16:41:20 +0100

    add functions

---
+++ functions/src/create-new-transcript.py
@@ -0,0 +1,57 @@
+import os
+import assemblyai as aai
+import json
+from datetime import datetime
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.exception import AppwriteException
+from appwrite.id import ID
+
+DB_ID = os.environ["DB_ID"]
+COLLECTION_ID = os.environ["COLLECTION_ID"]
+BUCKET_ID = os.environ["BUCKET_ID"]
+AAI_API_KEY = os.environ["AAI_API_KEY"]
+
+def main(context):
+
+    client = (
+        Client()
+        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+        .set_key(context.req.headers["x-appwrite-key"])
+    )
+    fileId = context.req.path.split('/')[-1]
+    context.log(json.dumps(context.req.query))  
+    name = context.req.query.get('name')
+    adventure = context.req.query.get('adventure')
+
+    try:
+        fileUrl = f"https://cloud.appwrite.io/v1/storage/buckets/{BUCKET_ID}/files/{fileId}/view?project={os.environ['APPWRITE_FUNCTION_PROJECT_ID']}"
+
+        aai.settings.api_key = AAI_API_KEY
+        transcriber = aai.Transcriber()
+
+        config = aai.TranscriptionConfig(speaker_labels=True)
+        #TODO implement language (NO HACKtoberfest2024)
+
+        transcript = transcriber.submit(fileUrl, config=config)
+        #TODO: verifica se c'è una sorta di timeout oppure no
+
+        db = Databases(client)
+
+        document_fields = {
+            "fileRef": fileId,
+            "dateUpload": datetime.now().isoformat(),
+            "fileUrl": fileUrl,
+            "isProcessing": True,
+            "assemblyAiTranscriptId": transcript.id,
+            "name":name,
+            "adventure":adventure
+        }
+        document_id = ID.unique()
+        document = db.create_document(DB_ID, COLLECTION_ID, document_id,document_fields)
+        return context.res.text(f"Document created with id {document_id}")
+    except (AppwriteException, Exception) as err:
+        context.error("Error: " + repr(err))
+        return context.res.text(f"Error: {err}")
+    
---
+++ functions/src/transcript-cron.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.exception import AppwriteException
+from appwrite.query import Query
+import os
+import requests
+
+DB_ID = os.environ["DB_ID"]
+BUCKET_ID = os.environ["BUCKET_ID"]
+COLLECTION_ID = os.environ["COLLECTION_ID"]
+AAI_API_KEY = os.environ["AAI_API_KEY"]
+
+def main(context):
+
+    client = (
+        Client()
+        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+        .set_key(context.req.headers["x-appwrite-key"])
+    )
+    cnt_transcripts_processed = 0
+    try:
+        db = Databases(client)
+        documents = db.list_documents(DB_ID, COLLECTION_ID, [
+            Query.equal("isProcessing", True)
+        ])
+
+        for document in documents['documents']:
+
+            response = requests.get(f"https://api.assemblyai.com/v2/transcript/{document['assemblyAiTranscriptId']}",
+                                    headers={"Authorization": AAI_API_KEY})
+            if response.status_code == 'completed':
+                context.log(response.json())
+                transcript = response.json();
+                transcript_text = ""
+                for utterance in transcript['utterances']:
+                    transcript_text += f"Speaker {utterance['speaker']}: {utterance['text']}\n"
+                if transcript is not None:
+                    db.update_document(DB_ID, COLLECTION_ID, document['$id'], {"rawText": transcript_text,
+                                                                               "isProcessing": False,
+                                                                               "dateUpload": datetime.now().isoformat()})
+                    cnt_transcripts_processed += 1
+            else:
+                context.log(f"Error getting transcript {document['assemblyAiTranscriptId']}")
+
+        message = f"Processed {cnt_transcripts_processed} transcripts"
+        context.log(message)
+        return context.res.text(message)
+    except (AppwriteException, Exception) as err:
+        context.log(f"Error: {err}")
+        return context.res.text(f"Error: {err}")
