9ed3bebbaaf19055b1e02798560ca583d1777d6f
ffex <kurogan21@gmail.com>
2025-01-17 12:25:11 +0100

    fix imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './menubar/menubar.component';
 import { PrimeNGConfig } from 'primeng/api';
 import { Aura } from 'primeng/themes/aura';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CommonModule } from '@angular/common';
-import { Nora } from 'primeng/themes/nora';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MenubarComponent,HomePageComponent],
+  imports: [CommonModule, RouterOutlet,  MenubarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
